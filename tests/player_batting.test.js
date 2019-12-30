e3831c0e10e2b73af8849f82e336a33385073779
Zach Smith <zsmith126028@gmail.com>
2019-12-30 17:12:05 -0600

    write initial test to determine if stats are being returned as json

--- tests/player_batting.test.js
+++ tests/player_batting.test.js
@@ -2,3 +2,17 @@ const sequelize = require("../config/database");
 const supertest = require("supertest");
 const app = require("../app");
 const api = supertest(app);
+
+describe("test routes for player batting statistics", () => {
+  test("all player hitting stats are returned as json", async () => {
+    console.log("Entered test...");
+    await api
+      .get("/api/batting")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+  });
+});
+
+afterAll(() => {
+  sequelize.close();
+});
