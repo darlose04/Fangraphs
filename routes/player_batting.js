5a346c3fc9e8689d4e64ff7c00229626d9d07cee
Zach Smith <zsmith126028@gmail.com>
2019-12-30 17:04:28 -0600

    begin creating batting routes

--- routes/player_batting.js
+++ routes/player_batting.js
@@ -2,3 +2,8 @@ const express = require("express");
 const router = express.Router();
 const PlayerBatting = require("../models/PlayerBatting");
 const Sequelize = require("sequelize");
+
+// get all player batting statistics
+router.get("/", (req, res));
+
+module.exports = router;
