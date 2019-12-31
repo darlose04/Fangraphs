1499a471f3a576ae636e8c9ef4496b5d603ffdfb
Zach Smith <zsmith126028@gmail.com>
2019-12-30 18:08:04 -0600

    add route that returns all player batting stats

--- routes/player_batting.js
+++ routes/player_batting.js
@@ -4,6 +4,10 @@ const PlayerBatting = require("../models/PlayerBatting");
 const Sequelize = require("sequelize");
 
 // get all player batting statistics
-// router.get("/", (req, res));
+router.get("/", (req, res) => {
+  PlayerBatting.findAll({
+    order: [["season", "ASC"]]
+  }).then(batters => res.json(batters));
+});
 
 module.exports = router;
