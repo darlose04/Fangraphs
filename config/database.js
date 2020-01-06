f228cd660afe2519d9cd0cd57796d873f46e17db
Zach Smith <zsmith126028@gmail.com>
2020-01-06 19:37:41 +0000

    change database option for production server

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize("baseballstats", "zach", process.env.PASSWORD, {
   host: "localhost",
-  dialect: "mariadb",
+  dialect: "mysql",
   dialectOptions: {
     timezone: "Etc/GMT+6"
   },
