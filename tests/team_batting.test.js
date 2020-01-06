333c20cf6c70323ebb763f6973ff28eca69adbd8
Zach Smith <zsmith126028@gmail.com>
2020-01-06 10:28:20 -0600

    remove test for initial team batting route after success

--- tests/team_batting.test.js
+++ tests/team_batting.test.js
@@ -3,10 +3,6 @@ const supertest = require("supertest");
 const app = require("../app");
 const api = supertest(app);
 
-test("test initial team batting route", async () => {
-  await api.get("/api/teambatting").expect(200);
-});
-
 afterAll(() => {
   sequelize.close();
 });
