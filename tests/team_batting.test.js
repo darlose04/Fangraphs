96d26975d8c1c4d2feab3b7c2135ecd774595d16
Zach Smith <zsmith126028@gmail.com>
2020-01-06 10:35:35 -0600

    add test to check if stats are returned as json. add test to make sure the proper number of items are returned

--- tests/team_batting.test.js
+++ tests/team_batting.test.js
@@ -3,6 +3,21 @@ const supertest = require("supertest");
 const app = require("../app");
 const api = supertest(app);
 
+describe("test route for all team batting statistics", () => {
+  test("all team batting stats are returned as json", async () => {
+    console.log("Entered test...");
+    await api
+      .get("/api/teambatting")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+  });
+
+  test("there are 540 entries returned", async () => {
+    const response = await api.get("/api/teambatting");
+    expect(response.body.length).toBe(540);
+  });
+});
+
 afterAll(() => {
   sequelize.close();
 });
