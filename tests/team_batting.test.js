178b998a1b443fc96098f5a96f08cbba31a9f488
Zach Smith <zsmith126028@gmail.com>
2020-01-06 10:42:07 -0600

    add test to make sure team batting stats are ordered by season and team name

--- tests/team_batting.test.js
+++ tests/team_batting.test.js
@@ -16,6 +16,19 @@ describe("test route for all team batting statistics", () => {
     const response = await api.get("/api/teambatting");
     expect(response.body.length).toBe(540);
   });
+
+  test("Returned statistics are ordered by season (ASC) and team (DESC)", async () => {
+    const response = await api.get("/api/teambatting");
+    let firstItem = response.body[0];
+    expect(firstItem.team).toBe("Angels");
+    expect(firstItem.season).toBe(2002);
+    let itemThirty = response.body[29];
+    expect(itemThirty.team).toBe("Yankees");
+    expect(itemThirty.season).toBe(2002);
+    let lastItem = reponse.body[reponse.body.length - 1];
+    expect(lastItem.team).toBe("Yankees");
+    expect(lastItem.season).toBe(2019);
+  });
 });
 
 afterAll(() => {
