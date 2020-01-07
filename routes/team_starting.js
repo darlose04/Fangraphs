65d9a23e0a6b7ba2cff0482a44273c7997cfdcb0
Zach Smith <zsmith126028@gmail.com>
2020-01-07 10:30:35 -0600

    add route to get all team starting stats

--- routes/team_starting.js
+++ routes/team_starting.js
@@ -3,4 +3,18 @@ const router = express.Router();
 const TeamStarting = require("../models/TeamStarting");
 const Sequelize = require("sequelize");
 
+// get all team starting pitching stats
+router.get("/", (req, res) => {
+  TeamStarting.findAll({
+    order: [
+      ["season", "ASC"],
+      ["team", "ASC"]
+    ]
+  }).then(teams => res.json(teams));
+});
+
+// get team starting pitching stats by season
+
+// get team starting pitching stats by team (returns info for that team across all seasons)
+
 module.exports = router;
