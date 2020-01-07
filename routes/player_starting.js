ed2e7881e9446081725160dfee093e3dc2674194
Zach Smith <zsmith126028@gmail.com>
2020-01-07 11:26:23 -0600

    add initial file startup

---
+++ routes/player_starting.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const router = express.Router();
+const PlayerStarting = require("../models/PlayerStarting");
+const Sequelize = require("sequelize");
+
+module.exports = router;
