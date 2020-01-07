1569b98fbfa67ef16030f5283eca3d91122923c6
Zach Smith <zsmith126028@gmail.com>
2020-01-07 11:55:29 -0600

    add route to return stats by team, by season

--- routes/player_starting.js
+++ routes/player_starting.js
@@ -43,4 +43,15 @@ router.get("/teams/:team", (req, res) => {
   }).then(teams => res.json(teams));
 });
 
+// get player starting pitching stats by team, by season
+router.get("/teams/:team/:season", (req, res) => {
+  PlayerStarting.findAll({
+    where: {
+      team: req.params.team,
+      season: req.params.season
+    },
+    order: [["war", "DESC"]]
+  }).then(teams => res.json(teams));
+});
+
 module.exports = router;
