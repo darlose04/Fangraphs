623d24472a4c2dfe9fec17af39cada1832b3b137
Zach Smith <zsmith126028@gmail.com>
2020-01-07 09:17:34 -0600

    import needed packages for team pitching routes

---
+++ routes/team_pitching.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const router = express.Router();
+const TeamPitching = require("../models/TeamPitching");
+const Sequelize = require("sequelize");
+
+module.exports = router;
