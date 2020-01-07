00141dfb35d48b42ef62435ff0c3857948d72009
Zach Smith <zsmith126028@gmail.com>
2020-01-07 09:29:33 -0600

    add comments. add route to return all team pitching stats

--- routes/team_pitching.js
+++ routes/team_pitching.js
@@ -3,4 +3,18 @@ const router = express.Router();
 const TeamPitching = require("../models/TeamPitching");
 const Sequelize = require("sequelize");
 
+// get all team pitching stats
+router.get("/", (req, res) => {
+  TeamPitching.findAll({
+    order: [
+      ["season", "ASC"],
+      ["team", "ASC"]
+    ]
+  }).then(teams => res.json(teams));
+});
+
+// find team pitching according to the season
+
+// find team pitching according to the team (returns info for that team across all seasons)
+
 module.exports = router;
