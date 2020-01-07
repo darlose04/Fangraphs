86720074b872f392b00e54783a208bb87c34684f
Zach Smith <zsmith126028@gmail.com>
2020-01-07 09:26:28 -0600

    add tests to check that stats are returned in json, returns all the items in the db, and items are ordered by season and team (alphabetically)

---
+++ tests/team_pitching.test.js
@@ -0,0 +1,36 @@
+const sequelize = require("../config/database");
+const supertest = require("supertest");
+const app = require("../app");
+const api = supertest(app);
+
+describe("test route for all team pitching statistics", () => {
+  test("all team pitching stats are returned as json", async () => {
+    console.log("Entered test...");
+    await api
+      .get("/api/teampitching")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+  });
+
+  test("there are 540 entries returned", async () => {
+    const response = await api.get("/api/teampitching");
+    expect(response.body.length).toBe(540);
+  });
+
+  test("Returned statistics are ordered by season (ASC) and team (ASC)", async () => {
+    const response = await api.get("/api/teampitching");
+    let firstItem = response.body[0];
+    expect(firstItem.team).toBe("Angels");
+    expect(firstItem.season).toBe(2002);
+    let itemThirty = response.body[29];
+    expect(itemThirty.team).toBe("Yankees");
+    expect(itemThirty.season).toBe(2002);
+    let lastItem = response.body[response.body.length - 1];
+    expect(lastItem.team).toBe("Yankees");
+    expect(lastItem.season).toBe(2019);
+  });
+});
+
+afterAll(() => {
+  sequelize.close();
+});
