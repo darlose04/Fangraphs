f71f3a624b8dc924e2beada806782c375d34d3dc
Zach Smith <zsmith126028@gmail.com>
2020-01-07 10:27:47 -0600

    add test to make sure stats are ordered by season and by team name

--- tests/team_starting.test.js
+++ tests/team_starting.test.js
@@ -13,8 +13,21 @@ describe("test route for returning all team starting pitching statistics", () =>
   });
 
   test("there are 540 entries returned", async () => {
-    const response = await api.get("/api/teampitching");
+    const response = await api.get("/api/teamstarting");
     expect(response.body.length).toBe(540);
+  });
+
+  test("Returned statistics are ordered by season (ASC) and team (ASC)", async () => {
+    const response = await api.get("/api/teamstarting");
+    let firstItem = response.body[0];
+    expect(firstItem.team).toBe("Angels");
+    expect(firstItem.season).toBe(2002);
+    let itemThirty = response.body[29];
+    expect(itemThirty.team).toBe("Yankees");
+    expect(itemThirty.season).toBe(2002);
+    let lastItem = response.body[response.body.length - 1];
+    expect(lastItem.team).toBe("Yankees");
+    expect(lastItem.season).toBe(2019);
   });
 });
 
